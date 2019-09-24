8d3d1479ced18fbe675dcfbf85d213baca325f09
Lichon <andrzej.lichon@bauer.pl>
2019-09-24 17:01:01 +0200

    add .env

--- src/settings.js
+++ src/settings.js
@@ -39,13 +39,13 @@ const settings = {
 
   credentials: {
     firebase: {
-      apiKey: '',
-      authDomain: '',
-      databaseURL: '',
-      projectId: '',
-      storageBucket: '',
-      messagingSenderId: '',
-      appId: ''
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_authDomain,
+      databaseURL: process.env.REACT_APP_databaseURL,
+      projectId: process.env.REACT_APP_projectId,
+      storageBucket: process.env.REACT_APP_storageBucket,
+      messagingSenderId: process.env.REACT_APP_messagingSenderId,
+      appId: process.env.REACT_APP_appId
     }
   }
 };
